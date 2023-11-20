ef3216986580bb2ae00666a03c04f0e4bf06f4e5
Marcos Rigel <marcosrigel99@gmail.com>
2023-11-20 17:43:44 -0300

    Deixando a content Page Dinamica

--- angular-blog/src/app/data/dataFake.ts
+++ angular-blog/src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
   {
-    "id": 1,
-    "photo": "aaa",
-    "title": "saiu novo filme",
-    "description": "ola"
+    "id": "1",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "marvel anuncia um novo filme do homem de ferro, confira",
+    "photoCover": "https://meups.com.br/wp-content/uploads/2022/09/Homem-de-Ferro.jpg"
   },
   {
-    "id": 2,
-    "photo": "aaa",
-    "title": "saiu novo filme",
-    "description": "ola"
+    "id": "2",
+    "title": "Nova Série anunciada no Disney +",
+    "description": "bla bla blaaaa",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
   }
 ]
